3fc5a435442357188ec1c3a8cbd7c33b5e47da3b
Suann <15521276768@163.com>
2018-11-20 01:25:49 +0800

    完成凑单界面结构及js

---
+++ js/coudan.js
@@ -0,0 +1,55 @@
+$(function(){
+    $('.dropdown-toggle').dropdown();
+    var shopid=0;
+    var areaid=0;
+    $.ajax({
+        type:'get',
+        url:'http://193.112.55.79:9090/api/getgsshop',
+        dataType:'json',
+        success:function(res){
+            var obj ={list:res.result} ;
+            var html = template('coudanShopTemp',obj);
+            $('#shopMess').html(html);               
+        }
+    })
+    $.ajax({
+        type:'get',
+        url:'http://193.112.55.79:9090/api/getgsshoparea',
+        dataType:'json',
+        success:function(res){
+            var obj ={list:res.result} ;
+            var html = template('coudanAreaTemp',obj);
+            $('#areaMess').html(html);    
+        }
+    })
+    $('#areaMess').on('click','li',function(){
+        areaid = $(this).attr('areaId');
+        $(this).addClass('active').siblings().removeClass('active');
+        getproduct();
+    })
+    $('#shopMess').on('click','li',function(){
+        shopid = $(this).attr('shopId');
+        $(this).addClass('active').siblings().removeClass('active');
+        getproduct();
+    })
+    getproduct();
+    function getproduct(){
+        $.ajax({
+            type:'get',
+            url:'http://193.112.55.79:9090/api/getgsproduct',
+            data: {
+                shopid:shopid,
+                areaid:areaid,
+            },
+            dataType:'json',
+            success:function(res){  
+                var obj ={list:res.result} ;
+                var html = template('coudanTemp',obj);
+                $('.coudan-product ul').html(html);
+            }
+        })
+    }
+   
+})
+
+          
